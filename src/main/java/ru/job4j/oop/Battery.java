50e36ce6b61d6974f27050bc55913c3f049d7c14
HEAVYxRAIN <heavyxrain@yandex.ru>
2021-10-12 19:16:00 +0300

    1.8. Состояние объекта [#167388]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -9,15 +9,15 @@ public class Battery {
     }
 
     public void exchange(Battery another) {
-        this.load = this.load - another.load;
-        another.load = 0;
+        another.load = this.load + another.load;
+        this.load = 0;
     }
 
     public static void main(String[] args) {
-        Battery charge = new Battery(100);
-        Battery discharged = new Battery(30);
-        System.out.println("charge : " + charge.load + ". discharged : " + discharged.load);
-        charge.exchange(discharged);
-        System.out.println("charge : " + charge.load + ". discharged : " + discharged.load);
+        Battery charge = new Battery(70);
+        Battery charger = new Battery(30);
+        System.out.println("charge : " + charge.load + ". charger : " + charger.load);
+        charge.exchange(charger);
+        System.out.println("charge : " + charge.load + ". charger : " + charger.load);
     }
 }
