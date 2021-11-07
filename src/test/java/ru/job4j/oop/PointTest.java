f6f0925ee7d998cefd053925318261483e17252b
HEAVYxRAIN <heavyxrain@yandex.ru>
2021-11-07 11:16:01 +0300

    2.2. Рефакторинг - Площадь треугольника. [#122642]

---
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.oop;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class PointTest {
+
+    @Test
+    public void when00too20then2() {
+        int expected = 2;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double out = new Point(x1, y1).distance(new Point(x2, y2));
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when10to20then1() {
+        int expected = 1;
+        int x1 = 1;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double out = new Point(x1, y1).distance(new Point(x2, y2));
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when30to50then2() {
+        int expected = 2;
+        int x1 = 3;
+        int y1 = 0;
+        int x2 = 5;
+        int y2 = 0;
+        double out = new Point(x1, y1).distance(new Point(x2, y2));
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when50to10then4() {
+        int expected = 4;
+        int x1 = 5;
+        int y1 = 0;
+        int x2 = 1;
+        int y2 = 0;
+        double out = new Point(x1, y1).distance(new Point(x2, y2));
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
