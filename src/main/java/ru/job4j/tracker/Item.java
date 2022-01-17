a169406c6a82ecd74079b7867be28da7c0b61845
HEAVYxRAIN <heavyxrain@yandex.ru>
2022-01-17 15:46:18 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,11 +1,14 @@
 package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Item {
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
+    private final DateTimeFormatter formatter =
+            DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
     public Item() {
     }
@@ -37,5 +40,11 @@ public class Item {
 
     public LocalDateTime getCreated() {
         return created;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" + "id=" + id + ", name='" + name + '\''
+                + ", created=" + created + ", formatter=" + formatter + '}';
     }
 }
--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,7 +6,8 @@ public class StartUI {
 
     public static void main(String[] args) {
         Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        DateTimeFormatter formatter =
+                DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String result = item.getCreated().format(formatter);
         System.out.println(result);
     }
