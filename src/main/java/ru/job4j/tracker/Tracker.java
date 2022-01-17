2bd0cccb067dc0ad69099b855c3a06575df644ce
HEAVYxRAIN <heavyxrain@yandex.ru>
2022-01-17 14:32:21 +0300

    8. Что такое валидация? [#246864]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -47,21 +47,22 @@ public class Tracker {
 
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
-        if (index != -1) {
+        boolean rsl = index != -1;
+        if (rsl) {
             item.setId(id);
             items[index] = item;
-            return true;
         }
-        return false;
+        return rsl;
     }
 
     public boolean delete(int ids) {
         int index = indexOf(ids);
-        if (index != -1) {
+        boolean rsl = index != -1;
+        if (rsl) {
             System.arraycopy(items, index + 1, items, index,
                     size - index - 1);
             size--;
         }
-        return  index != -1;
+        return  rsl;
     }
 }
