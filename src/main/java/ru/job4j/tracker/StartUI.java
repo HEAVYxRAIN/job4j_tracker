13f4828ab8d98fd93ba60a780c75c040f7fd4c3c
HEAVYxRAIN <heavyxrain@yandex.ru>
2022-07-29 17:35:53 +0300

    6. Изменить программу Tracker из 2-го модуля [#10039]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,5 +1,10 @@
 package ru.job4j.tracker;
 
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class StartUI {
     private final Output out;
 
@@ -7,24 +12,26 @@ public class StartUI {
         this.out = out;
     }
 
-    public void init(Input input, Tracker tracker, UserAction[] actions) {
+    public void init(Input input, Tracker tracker, List<UserAction> actions) {
         boolean run = true;
         while (run) {
             this.showMenu(actions);
             int select = input.askInt("Select: ");
-            if (select < 0 || select >= actions.length) {
-                out.println("Wrong input, you can select: 0 .. " + (actions.length - 1));
+            if (select < 0 || select >= actions.size()) {
+                out.println("Wrong input, you can select: 0 .. " + (actions.size()));
                 continue;
             }
-            UserAction action = actions[select];
+            UserAction action = actions.get(select);
             run = action.execute(input, tracker);
         }
     }
 
-    private void showMenu(UserAction[] actions) {
+    private void showMenu(List<UserAction> actions) {
         out.println("Menu.");
-        for (int index = 0; index < actions.length; index++) {
-            out.println(index + ". " + actions[index].name());
+        int index = 0;
+        for (UserAction action : actions) {
+            out.println(index + ". " + action.name());
+            index++;
         }
     }
 
@@ -32,12 +39,10 @@ public class StartUI {
         Output output = new ConsoleOutput();
         Input input = new ValidateInput(output, new ConsoleInput());
         Tracker tracker = new Tracker();
-        UserAction[] actions = {
-                new CreateAction(output), new ShowAllAction(output),
-                new EditAction(output), new DeleteAction(output),
-                new FindIdAction(output), new FindNameAction(output),
-                new ExitAction(output)
-        };
+        List<UserAction> actions = List.of(new CreateAction(output),
+                new ShowAllAction(output), new EditAction(output),
+                new DeleteAction(output), new FindIdAction(output),
+                new FindNameAction(output), new ExitAction(output));
         new StartUI(output).init(input, tracker, actions);
     }
 }
